3954c766ecea4a2dd9e65b7428b683f59bc7c15d
vamshi <vamshikrishnam95@gmail.com>
2021-04-29 18:40:21 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("vamshi", 10000);
+            Console.WriteLine("Hi {0} your bank balance is {1} and account number {2}", 
+                account.OwnerName, account.Balance, account.Number);
+
+            account.MakeWithdrawl(100, DateTime.Now, "withdrawl");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.getTransactions());
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
